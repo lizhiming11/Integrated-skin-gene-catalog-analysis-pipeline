c2d6d2af97a55e6bd1087ddf8ab1a0ea27e4211c
lizm <642998881@qq.com>
2020-08-25 14:20:08 +0800

    Update gene_quantigication.sh

--- Quantification of gene, KO and species/gene_quantigication.sh
+++ Quantification of gene, KO and species/gene_quantigication.sh
@@ -4,6 +4,6 @@ fasta_length -i gene_catalog.fas | awk '{print NR"\t"$0}'>  gene_catalog.fas.len
 sample = $1
 perl abundance.pl -a $sample_rmHost.1.fq.gz -b $sample_rmHost.2.fq.gz -d gene_catalog.fas.index -g  gene_catalog.fas.length -p ./abundance/$sample
 
-profile_v1.0 -i skin.list -p gene_profile
+profile_v1.0 -i skin_list -p gene_profile
 
 
