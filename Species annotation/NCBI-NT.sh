c1d53fb2ecfdbbba8ea5912a32e0c428382277e2
lizm <642998881@qq.com>
2020-08-25 11:15:41 +0800

    Update NCBI-NT.sh

--- Species annotation/NCBI-NT.sh
+++ Species annotation/NCBI-NT.sh
@@ -2,3 +2,5 @@
 perl split_fa.pl s 9 ../gene_catalog.fas split
 blastn -word_size 16 -query split_1.fa -out split_1.bt -db NCBI-NT.fa -evalue 1e-10 -outfmt 6 -num_threads 20
 perl get_length.pl gene_catalog.fas.pep gene_catalog.fas.pep.len
+filter_blast -i split_1.bt -o split_1.bt.f --qfile gene_catalog.fas.pep.len --qper 70 --tops 5
+perl get_species_anno.v2019.pl split_1.bt.f split_1.bt.f.tax 
