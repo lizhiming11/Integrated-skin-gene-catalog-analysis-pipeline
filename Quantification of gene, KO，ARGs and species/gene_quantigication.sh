2951d476686d4f271145d5006eb9dd45d71eb698
lizm <642998881@qq.com>
2020-08-25 16:18:47 +0800

    Update gene_quantigication.sh

--- Quantification of gene, KO，ARGs and species/gene_quantigication.sh
+++ Quantification of gene, KO，ARGs and species/gene_quantigication.sh
@@ -5,3 +5,9 @@ sample = $1
 perl abundance.pl -a $sample_rmHost.1.fq.gz -b $sample_rmHost.2.fq.gz -d gene_catalog.fas.index -g  gene_catalog.fas.length -p ./abundance/$sample
 
 profile_v1.0 -i skin_list -p gene_profile
+
+perl genecatalog-profile2ko.pl -i gene_profile -o species.profile -l gene_catalog.fas.length -a NT.tax
+
+perl genecatalog-profile2ko.pl -i gene_profile -o ko.profile -l gene_catalog.fas.length -a ko.ann
+
+perl genecatalog-profile2ko.pl -i gene_profile -o card.profile -l gene_catalog.fas.length -a card.ann
